9b62b4dfa4b0204a7409df597e4710e54e235079
josephexequiel <52370507+josephexequiel@users.noreply.github.com>
2021-04-07 20:27:08 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+A = [1, 0, 1, 0, 1, 0, 1];
+
+def looper(baseval, tempval):
+    if baseval == 0:
+        if tempval == 1:
+            return True;
+        else:
+            return False;
+    elif baseval == 1:
+        if tempval == 0:
+            return True;
+        else:
+            return False;
+    else:
+        return False;
+
+def solution(A):
+    i = 0;
+    count = 0;
+    baseval = A[0];
+    while i < len(A):
+        if looper(baseval, A[i]) == False:
+            count = count + 1;
+            if baseval == 0:
+                baseval = 1;
+            elif baseval == 1:
+                baseval = 0;
+        else:
+            baseval = A[i];
+        i = i + 1;
+    print(len(A) - count);
+
+solution(A);
